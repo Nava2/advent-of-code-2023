a8626b39f5c2f8f9cfa5011daafe758b2d3a105e
Kevin Brightwell <kevin.brightwell@faire.com>
2023-12-02 06:27:23 -0500

    skeleton

---
+++ src/test/kotlin/net/navatwo/adventofcode2023/Day2SolutionTest.kt
@@ -0,0 +1,50 @@
+package net.navatwo.adventofcode2023
+
+import net.navatwo.adventofcode2023.benchmarks.Benchmark
+import net.navatwo.adventofcode2023.day2.Day2Solution
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day2SolutionTest {
+    @Test
+    fun `p1 sample`() {
+        val resourceName = "day1/p1_sample.txt"
+        val solution = Day2Solution.Part1
+        val input = solution.parseResource(resourceName)
+        assertThat(Day2Solution.Part1.solve(input)).isComputed(142L)
+    }
+
+    @Test
+    fun `p1`() {
+        val resourceName = "day1/p1_input.txt"
+        val solution = Day2Solution.Part1
+        val input = solution.parseResource(resourceName)
+        assertThat(Day2Solution.Part1.solve(input)).isComputed(55002L)
+
+        Benchmark.run(
+            inputContent = loadText(resourceName),
+            solution = solution,
+        )
+    }
+
+    @Test
+    fun `p2 sample`() {
+        val resourceName = "day1/p2_sample.txt"
+        val solution = Day2Solution.Part2
+        val input = solution.parseResource(resourceName)
+        assertThat(Day2Solution.Part2.solve(input)).isComputed(281)
+    }
+
+    @Test
+    fun `p2`() {
+        val resourceName = "day1/p1_input.txt"
+        val solution = Day2Solution.Part2
+        val input = solution.parseResource(resourceName)
+        assertThat(Day2Solution.Part2.solve(input)).isComputed(55093L)
+
+        Benchmark.run(
+            inputContent = loadText(resourceName),
+            solution = solution,
+        )
+    }
+}
