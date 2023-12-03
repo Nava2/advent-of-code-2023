689e21d318859a79553c9f8678eb9ca3e3b5e413
Kevin Brightwell <kevin.brightwell@faire.com>
2023-12-03 09:39:34 -0500

    day 3: parsing

--- src/test/kotlin/net/navatwo/adventofcode2023/day3/Day3SolutionTest.kt
+++ src/test/kotlin/net/navatwo/adventofcode2023/day3/Day3SolutionTest.kt
@@ -1,11 +1,8 @@
 package net.navatwo.adventofcode2023.day3
 
-import net.navatwo.adventofcode2023.benchmarks.Benchmark
-import net.navatwo.adventofcode2023.day2.Day2Solution
-import net.navatwo.adventofcode2023.day2.Day2Solution.Game
-import net.navatwo.adventofcode2023.day2.Day2Solution.Game.Colour
-import net.navatwo.adventofcode2023.isComputed
-import net.navatwo.adventofcode2023.loadText
+import net.navatwo.adventofcode2023.Coord
+import net.navatwo.adventofcode2023.day3.Day3Solution.EngineCoord
+import net.navatwo.adventofcode2023.get
 import net.navatwo.adventofcode2023.parseResource
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -13,71 +10,64 @@ import org.junit.jupiter.api.Test
 class Day3SolutionTest {
   @Test
   fun `p1 parse`() {
-    val resourceName = "day2/p1_sample.txt"
-    val solution = Day2Solution.Part1
-    val input = solution.parseResource(resourceName)
-    assertThat(input).hasSize(5)
-    assertThat(input[0]).isEqualTo(
-      Game(
-        id = 1,
-        pulls = listOf(
-          Game.Pull(mapOf(Colour.Blue to 3, Colour.Red to 4)),
-          Game.Pull(mapOf(Colour.Red to 1, Colour.Green to 2, Colour.Blue to 6)),
-          Game.Pull(mapOf(Colour.Green to 2)),
-        )
-      )
-    )
-    assertThat(input[3]).isEqualTo(
-      Game(
-        id = 4,
-        pulls = listOf(
-          Game.Pull(mapOf(Colour.Green to 1, Colour.Red to 3, Colour.Blue to 6)),
-          Game.Pull(mapOf(Colour.Green to 3, Colour.Red to 6)),
-          Game.Pull(mapOf(Colour.Green to 3, Colour.Blue to 15, Colour.Red to 14)),
-        )
-      )
-    )
+    val resourceName = "day3/p1_sample.txt"
+    val solution = Day3Solution.Part1
+    val schematic = solution.parseResource(resourceName)
+    assertThat(schematic.grid).hasSize(10)
+      .allSatisfy { assertThat(it).hasSize(10) }
+
+    assertThat(schematic.grid[Coord(0, 0)])
+      .isEqualTo(EngineCoord(Coord(0, 0), '4'))
+    assertThat(schematic.grid[Coord(1, 0)])
+      .isEqualTo(EngineCoord(Coord(1, 0), '6'))
+    assertThat(schematic.grid[Coord(2, 0)])
+      .isEqualTo(EngineCoord(Coord(2, 0), '7'))
+    assertThat(schematic.grid[Coord(3, 0)])
+      .isEqualTo(EngineCoord(Coord(3, 0), '.'))
+
+    assertThat(schematic.grid[Coord(3, 1)])
+      .isEqualTo(EngineCoord(Coord(3, 1), '*'))
   }
 
-  @Test
-  fun `p1 sample`() {
-    val resourceName = "day2/p1_sample.txt"
-    val solution = Day2Solution.Part1
-    val input = solution.parseResource(resourceName)
-    assertThat(Day2Solution.Part1.solve(input)).isComputed(8)
-  }
-
-  @Test
-  fun `p1`() {
-    val resourceName = "day2/p1_input.txt"
-    val solution = Day2Solution.Part1
-    val input = solution.parseResource(resourceName)
-    assertThat(Day2Solution.Part1.solve(input)).isComputed(2447)
-
-    Benchmark.run(
-      inputContent = loadText(resourceName),
-      solution = solution,
-    )
-  }
-
-  @Test
-  fun `p2 sample`() {
-    val resourceName = "day2/p1_sample.txt"
-    val solution = Day2Solution.Part2
-    val input = solution.parseResource(resourceName)
-    assertThat(Day2Solution.Part2.solve(input)).isComputed(2286)
-  }
-
-  @Test
-  fun `p2`() {
-    val resourceName = "day2/p1_input.txt"
-    val solution = Day2Solution.Part2
-    val input = solution.parseResource(resourceName)
-    assertThat(Day2Solution.Part2.solve(input)).isComputed(56322L)
-
-    Benchmark.run(
-      inputContent = loadText(resourceName),
-      solution = solution,
-    )
-  }
+//  @Test
+//  fun `p1 sample`() {
+//    val resourceName = "day3/p1_sample.txt"
+//    val solution = Day3Solution.Part1
+//    val input = solution.parseResource(resourceName)
+//    assertThat(Day3Solution.Part1.solve(input)).isComputed(8)
+//  }
+//
+//  @Test
+//  fun `p1`() {
+//    val resourceName = "day3/p1_input.txt"
+//    val solution = Day3Solution.Part1
+//    val input = solution.parseResource(resourceName)
+//    assertThat(Day3Solution.Part1.solve(input)).isComputed(2447)
+//
+//    Benchmark.run(
+//      inputContent = loadText(resourceName),
+//      solution = solution,
+//    )
+//  }
+//
+//  @Test
+//  fun `p2 sample`() {
+//    val resourceName = "day3/p1_sample.txt"
+//    val solution = Day3Solution.Part2
+//    val input = solution.parseResource(resourceName)
+//    assertThat(Day3Solution.Part2.solve(input)).isComputed(2286)
+//  }
+//
+//  @Test
+//  fun `p2`() {
+//    val resourceName = "day3/p1_input.txt"
+//    val solution = Day3Solution.Part2
+//    val input = solution.parseResource(resourceName)
+//    assertThat(Day3Solution.Part2.solve(input)).isComputed(56322L)
+//
+//    Benchmark.run(
+//      inputContent = loadText(resourceName),
+//      solution = solution,
+//    )
+//  }
 }
