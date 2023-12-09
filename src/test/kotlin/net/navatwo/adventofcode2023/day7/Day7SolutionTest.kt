fcad753bf73cddc5c9e73898e6956488e4657e8c
Kevin Brightwell <kevin.brightwell@faire.com>
2023-12-09 11:03:35 -0500

    initial generation

---
+++ src/test/kotlin/net/navatwo/adventofcode2023/day7/Day7SolutionTest.kt
@@ -0,0 +1,60 @@
+package net.navatwo.adventofcode2023.day7
+
+import net.navatwo.adventofcode2023.benchmarks.Benchmark
+import net.navatwo.adventofcode2023.isComputed
+import net.navatwo.adventofcode2023.loadText
+import net.navatwo.adventofcode2023.parseResource
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class Day7SolutionTest {
+  @Test
+  fun `p1 parse`() {
+    val resourceName = "day7/p1_sample.txt"
+    val solution = Day7Solution.Part1
+    val inputs = solution.parseResource(resourceName)
+    assertThat(inputs).isNotEmpty()
+  }
+
+  @Test
+  fun `p1 sample`() {
+    val resourceName = "day7/p1_sample.txt"
+    val solution = Day7Solution.Part1
+    val input = solution.parseResource(resourceName)
+    assertThat(Day7Solution.Part1.solve(input)).isComputed(32L)
+  }
+
+  @Test
+  fun `p1`() {
+    val resourceName = "day7/p1_input.txt"
+    val solution = Day7Solution.Part1
+    val input = solution.parseResource(resourceName)
+    assertThat(Day7Solution.Part1.solve(input)).isComputed(32L)
+
+    Benchmark.run(
+      inputContent = loadText(resourceName),
+      solution = solution,
+    )
+  }
+
+//  @Test
+//  fun `p2 sample`() {
+//    val resourceName = "day7/p1_sample.txt"
+//    val solution = Day7Solution.Part2
+//    val input = solution.parseResource(resourceName)
+//    assertThat(Day7Solution.Part2.solve(input)).isComputed(46)
+//  }
+//
+//  @Test
+//  fun `p2`() {
+//    val resourceName = "day7/p1_input.txt"
+//    val solution = Day7Solution.Part2
+//    val input = solution.parseResource(resourceName)
+//    assertThat(Day7Solution.Part2.solve(input)).isComputed(5132675L)
+//
+//    Benchmark.run(
+//      inputContent = loadText(resourceName),
+//      solution = solution,
+//    )
+//  }
+}
