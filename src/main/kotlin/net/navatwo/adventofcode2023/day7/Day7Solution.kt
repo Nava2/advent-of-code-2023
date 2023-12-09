2d33d2f397602a53ff2999a03f6e6d7a3af0ae30
Kevin Brightwell <kevin.brightwell2@gmail.com>
2023-12-09 13:23:53 -0500

    Day 7: Are camels good card players? (#7)

---
+++ src/main/kotlin/net/navatwo/adventofcode2023/day7/Day7Solution.kt
@@ -0,0 +1,198 @@
+package net.navatwo.adventofcode2023.day7
+
+import net.navatwo.adventofcode2023.framework.ComputedResult
+import net.navatwo.adventofcode2023.framework.Solution
+
+sealed class Day7Solution : Solution<Day7Solution.Game> {
+  internal abstract val handComparator: Comparator<Hand>
+
+  // lazy due to accessing `abstract val` property
+  private val tripleComparator: Comparator<ComputedTriple> by lazy(LazyThreadSafetyMode.NONE) {
+    Comparator
+      .comparing<ComputedTriple, HandType> { it.handType }
+      .thenBy(handComparator) { it.hand }
+  }
+
+  internal abstract fun computeHandType(hand: Hand): HandType
+
+  override fun solve(input: Game): ComputedResult {
+    val computeHandTypes = input.handPairs.asSequence()
+      .map { (hand, bid) ->
+        ComputedTriple(hand, computeHandType(hand), bid)
+      }
+
+    val rankedHands = computeHandTypes.toSortedSet(tripleComparator.reversed())
+
+    val rankedBids = rankedHands.asSequence().map { it.bid }
+
+    val result = rankedBids.withIndex()
+      .fold(0L) { acc, (index, bid) ->
+        acc + (index + 1) * bid.value
+      }
+    return ComputedResult.Simple(result)
+  }
+
+  data object Part1 : Day7Solution() {
+    internal val cardComparator: Comparator<PlayingCard> = Comparator.comparing { it.ordinal }
+    override val handComparator = Hand.comparator(cardComparator)
+
+    override fun computeHandType(hand: Hand): HandType {
+      val cardFrequencies = hand.countCards()
+      return HandType.computeFromCardFrequencies(cardFrequencies)
+    }
+  }
+
+  data object Part2 : Day7Solution() {
+
+    internal val cardComparator: Comparator<PlayingCard> = Comparator.comparing { card ->
+      if (card == PlayingCard.Jack) {
+        PlayingCard.Two.ordinal + 50 // really after 2
+      } else {
+        card.ordinal
+      }
+    }
+
+    override val handComparator = Hand.comparator(cardComparator)
+
+    override fun computeHandType(hand: Hand): HandType {
+      val cardFrequencies = hand.countCards()
+
+      val jackCount = cardFrequencies[PlayingCard.Jack]
+        // No jacks => no need to upgrade them
+        ?: return HandType.computeFromCardFrequencies(cardFrequencies)
+
+      // update most frequent, non-Jack card to add count of jacks or add Ace with jack count otherwise
+      val mostFrequentCard = cardFrequencies.entries.asSequence()
+        .filter { (card, _) -> card != PlayingCard.Jack }
+        .sortedWith { p1, p2 ->
+          val (card1, count1) = p1
+          val (card2, count2) = p2
+
+          val countComparison = count2.compareTo(count1)
+          if (countComparison != 0) return@sortedWith countComparison
+
+          cardComparator.compare(card1, card2)
+        }
+        .maxByOrNull { (_, count) -> count }?.key
+        ?: PlayingCard.Ace
+
+      val newFrequencies = cardFrequencies.apply {
+        remove(PlayingCard.Jack)
+        compute(mostFrequentCard) { _, count ->
+          (count ?: 0) + jackCount
+        }
+      }
+
+      return HandType.computeFromCardFrequencies(newFrequencies)
+    }
+  }
+
+  override fun parse(lines: Sequence<String>): Game {
+    val handPairs = lines.map { line ->
+      val (hand, bid) = line.split(" ", limit = 2)
+      Hand.parse(hand) to Bid(bid.toLong())
+    }
+
+    return Game(handPairs.toList())
+  }
+
+  data class ComputedTriple(
+    val hand: Hand,
+    val handType: HandType,
+    val bid: Bid,
+  ) {
+    companion object
+  }
+
+  @JvmInline
+  value class Hand(val cards: List<PlayingCard>) {
+    fun countCards(): MutableMap<PlayingCard, Int> = cards.fold(mutableMapOf()) { acc, card ->
+      acc.compute(card) { _, count ->
+        (count ?: 0) + 1
+      }
+      acc
+    }
+
+    override fun toString(): String = cards.joinToString(separator = "") { it.shortName.toString() }
+
+    companion object {
+      fun comparator(cardComparator: Comparator<PlayingCard>): Comparator<Hand> = Comparator { h1, h2 ->
+        for ((c1, c2) in h1.cards.asSequence().zip(h2.cards.asSequence())) {
+          val cardComparison = cardComparator.compare(c1, c2)
+          if (cardComparison != 0) return@Comparator cardComparison
+        }
+
+        0
+      }
+
+      fun parse(line: CharSequence): Hand {
+        val cards = line.map { PlayingCard.byShortName(it) }
+        return Hand(cards)
+      }
+    }
+  }
+
+  @JvmInline
+  value class Bid(val value: Long)
+
+  @JvmInline
+  value class Game(val handPairs: List<Pair<Hand, Bid>>)
+
+  enum class PlayingCard(val shortName: Char) : Comparable<PlayingCard> {
+    Ace('A'),
+    King('K'),
+    Queen('Q'),
+    Jack('J'),
+    Ten('T'),
+    Nine('9'),
+    Eight('8'),
+    Seven('7'),
+    Six('6'),
+    Five('5'),
+    Four('4'),
+    Three('3'),
+    Two('2'),
+    ;
+
+    companion object {
+      private val byShortName = entries.associateBy { it.shortName }
+
+      fun byShortName(shortName: Char): PlayingCard {
+        return byShortName.getValue(shortName)
+      }
+    }
+  }
+
+  enum class HandType {
+    FiveOfAKind,
+    FourOfAKind,
+    FullHouse,
+    ThreeOfAKind,
+    TwoPair,
+    OnePair,
+    HighCard,
+    ;
+
+    companion object {
+      fun computeFromCardFrequencies(cardFrequencies: Map<PlayingCard, Int>): HandType {
+        val (highestCountCard, highestCount) = cardFrequencies.entries.maxBy { (_, count) -> count }
+
+        return when {
+          cardFrequencies.size == 1 -> FiveOfAKind
+          cardFrequencies.size == 2 -> {
+            if (highestCount in 2..3) {
+              FullHouse
+            } else {
+              FourOfAKind
+            }
+          }
+
+          cardFrequencies.size == 3 && highestCount == 3 -> ThreeOfAKind
+          cardFrequencies.size == 3 && highestCount == 2 -> TwoPair
+          cardFrequencies.size == 4 -> OnePair
+          else -> HighCard
+        }
+      }
+    }
+  }
+}
---
+++ src/test/kotlin/net/navatwo/adventofcode2023/day7/Day7SolutionTest.kt
@@ -0,0 +1,225 @@
+package net.navatwo.adventofcode2023.day7
+
+import net.navatwo.adventofcode2023.benchmarks.Benchmark
+import net.navatwo.adventofcode2023.day7.Day7Solution.Bid
+import net.navatwo.adventofcode2023.day7.Day7Solution.Game
+import net.navatwo.adventofcode2023.day7.Day7Solution.Hand
+import net.navatwo.adventofcode2023.day7.Day7Solution.HandType
+import net.navatwo.adventofcode2023.day7.Day7Solution.Part1
+import net.navatwo.adventofcode2023.day7.Day7Solution.Part2
+import net.navatwo.adventofcode2023.day7.Day7Solution.Part2.cardComparator
+import net.navatwo.adventofcode2023.day7.Day7Solution.PlayingCard
+import net.navatwo.adventofcode2023.isComputed
+import net.navatwo.adventofcode2023.loadText
+import net.navatwo.adventofcode2023.parseResource
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
+
+class Day7SolutionTest {
+  @Test
+  fun `p1 parse`() {
+    val resourceName = "day7/p1_sample.txt"
+    val solution = Part1
+    val game = solution.parseResource(resourceName)
+    assertThat(game).isEqualTo(
+      Game(
+        handPairs = listOf(
+          Hand.parse("32T3K") to Bid(765),
+          Hand.parse("T55J5") to Bid(684),
+          Hand.parse("KK677") to Bid(28),
+          Hand.parse("KTJJT") to Bid(220),
+          Hand.parse("QQQJA") to Bid(483),
+        )
+      )
+    )
+  }
+
+  @Test
+  fun `p1 sample`() {
+    val resourceName = "day7/p1_sample.txt"
+    val solution = Part1
+    val input = solution.parseResource(resourceName)
+    assertThat(Part1.solve(input)).isComputed(6440L)
+  }
+
+  @Test
+  fun `p1`() {
+    val resourceName = "day7/p1_input.txt"
+    val solution = Part1
+    val input = solution.parseResource(resourceName)
+    assertThat(Part1.solve(input)).isComputed(250232501L)
+
+    Benchmark.run(
+      inputContent = loadText(resourceName),
+      solution = solution,
+    )
+  }
+
+  @Test
+  fun `p2 sample`() {
+    val resourceName = "day7/p1_sample.txt"
+    val solution = Part2
+    val input = solution.parseResource(resourceName)
+    assertThat(Part2.solve(input)).isComputed(5905)
+  }
+
+  @Test
+  fun `p2`() {
+    val resourceName = "day7/p1_input.txt"
+    val solution = Part2
+    val input = solution.parseResource(resourceName)
+    assertThat(Part2.solve(input)).isComputed(249138943L)
+
+    Benchmark.run(
+      inputContent = loadText(resourceName),
+      solution = solution,
+    )
+  }
+
+  @Test
+  fun `hand parse`() {
+    val hand = Hand.parse("AKQJT98765432")
+    assertThat(hand.cards).containsExactly(
+      PlayingCard.Ace,
+      PlayingCard.King,
+      PlayingCard.Queen,
+      PlayingCard.Jack,
+      PlayingCard.Ten,
+      PlayingCard.Nine,
+      PlayingCard.Eight,
+      PlayingCard.Seven,
+      PlayingCard.Six,
+      PlayingCard.Five,
+      PlayingCard.Four,
+      PlayingCard.Three,
+      PlayingCard.Two,
+    )
+  }
+
+  @ParameterizedTest(name = "hand = {0}, expected = {1}")
+  @MethodSource("part1HandTypeTestProvider")
+  fun `p1 compute handType`(handString: String, expected: HandType) {
+    val hand = Hand.parse(handString)
+
+    assertThat(Part1.computeHandType(hand)).isEqualTo(expected)
+  }
+
+  @Test
+  fun `p1 card sorting`() {
+    val playingCardsUnsorted = listOf(
+      PlayingCard.Ace,
+      PlayingCard.Jack,
+      PlayingCard.Ten,
+      PlayingCard.Two,
+    )
+
+    assertThat(playingCardsUnsorted.sortedWith(Part1.cardComparator)).containsExactly(
+      PlayingCard.Ace,
+      PlayingCard.Jack,
+      PlayingCard.Ten,
+      PlayingCard.Two,
+    )
+  }
+
+  @ParameterizedTest(name = "hand = {0}, expected = {1}")
+  @MethodSource("part2HandTypeTestProvider")
+  fun `p2 compute handType`(handString: String, expected: HandType) {
+    val hand = Hand.parse(handString)
+
+    assertThat(Part2.computeHandType(hand)).isEqualTo(expected)
+  }
+
+  @Test
+  fun `p2 card sorting`() {
+    val playingCardsUnsorted = listOf(
+      PlayingCard.Ace,
+      PlayingCard.Jack,
+      PlayingCard.Ten,
+      PlayingCard.Two,
+    )
+
+    assertThat(playingCardsUnsorted.sortedWith(cardComparator)).containsExactly(
+      PlayingCard.Ace,
+      PlayingCard.Ten,
+      PlayingCard.Two,
+      PlayingCard.Jack,
+    )
+  }
+
+  @Test
+  fun `p2 hand sorting`() {
+    val aces = Hand.parse("AAAAA")
+    val jokers = Hand.parse("JJJJJ")
+    val twos = Hand.parse("22222")
+
+    assertThat(listOf(aces, jokers, twos).sortedWith(Part2.handComparator))
+      .containsExactly(
+        aces,
+        twos,
+        jokers,
+      )
+  }
+
+  companion object {
+    @JvmStatic
+    fun part1HandTypeTestProvider(): List<Array<Any>> = listOf(
+      arrayOf("AAAAA", HandType.FiveOfAKind),
+
+      arrayOf("AAAAJ", HandType.FourOfAKind),
+      arrayOf("JAAAA", HandType.FourOfAKind),
+      arrayOf("AJAAA", HandType.FourOfAKind),
+
+      arrayOf("AA222", HandType.FullHouse),
+      arrayOf("A22A2", HandType.FullHouse),
+      arrayOf("22AA2", HandType.FullHouse),
+
+      arrayOf("22QA2", HandType.ThreeOfAKind),
+      arrayOf("22245", HandType.ThreeOfAKind),
+
+      arrayOf("2244A", HandType.TwoPair),
+      arrayOf("24A2A", HandType.TwoPair),
+
+      arrayOf("24572", HandType.OnePair),
+      arrayOf("AA425", HandType.OnePair),
+
+      arrayOf("98746", HandType.HighCard),
+    )
+
+    @JvmStatic
+    fun part2HandTypeTestProvider(): List<Array<Any>> = listOf(
+      arrayOf("JJJJJ", HandType.FiveOfAKind),
+
+      arrayOf("AAAAJ", HandType.FiveOfAKind),
+      arrayOf("AAAJJ", HandType.FiveOfAKind),
+      arrayOf("AAJJJ", HandType.FiveOfAKind),
+      arrayOf("AJJJJ", HandType.FiveOfAKind),
+      arrayOf("AAAAA", HandType.FiveOfAKind),
+
+      arrayOf("AAAAQ", HandType.FourOfAKind),
+      arrayOf("AAAJQ", HandType.FourOfAKind),
+      arrayOf("QJAAA", HandType.FourOfAKind),
+      arrayOf("QJJJA", HandType.FourOfAKind),
+
+      arrayOf("AA222", HandType.FullHouse),
+      arrayOf("AA2J2", HandType.FullHouse),
+      arrayOf("A22A2", HandType.FullHouse),
+      arrayOf("AJ2A2", HandType.FullHouse),
+      arrayOf("22AA2", HandType.FullHouse),
+
+      arrayOf("22QA2", HandType.ThreeOfAKind),
+      arrayOf("2JQA2", HandType.ThreeOfAKind),
+      arrayOf("J2245", HandType.ThreeOfAKind),
+
+      arrayOf("2244A", HandType.TwoPair),
+      arrayOf("24A2A", HandType.TwoPair),
+
+      arrayOf("2457J", HandType.OnePair),
+      arrayOf("24572", HandType.OnePair),
+      arrayOf("AA425", HandType.OnePair),
+
+      arrayOf("98746", HandType.HighCard),
+    )
+  }
+}
