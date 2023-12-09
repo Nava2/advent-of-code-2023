15e530eb5987b071933d6a05f6b392e81ea4bdea
Kevin Brightwell <kevin.brightwell@faire.com>
2023-12-09 14:19:24 -0500

    parse input

--- src/main/kotlin/net/navatwo/adventofcode2023/day8/Day8Solution.kt
+++ src/main/kotlin/net/navatwo/adventofcode2023/day8/Day8Solution.kt
@@ -3,18 +3,63 @@ package net.navatwo.adventofcode2023.day8
 import net.navatwo.adventofcode2023.framework.ComputedResult
 import net.navatwo.adventofcode2023.framework.Solution
 
-sealed class Day8Solution : Solution<List<Day8Solution.Input>> {
+sealed class Day8Solution : Solution<Day8Solution.Input> {
   data object Part1 : Day8Solution() {
-    override fun solve(input: List<Input>): ComputedResult {
+    override fun solve(input: Input): ComputedResult {
       // TODO Implement
       return ComputedResult.Simple(32L)
     }
   }
 
-  override fun parse(lines: Sequence<String>): List<Input> {
-    return lines.map { Input(it) }.toList()
+  override fun parse(lines: Sequence<String>): Input {
+    var directions: Directions? = null
+
+    val nodes = mutableListOf<NodeData>()
+    for (line in lines.filter { it.isNotBlank() }) {
+      if (directions == null) {
+        val directionList = line.map { char ->
+          when (char) {
+            'L' -> Direction.Left
+            'R' -> Direction.Right
+            else -> throw IllegalArgumentException("Invalid direction: $char")
+          }
+        }
+        directions = Directions(directionList)
+      } else {
+        val name = NodeName(line.substring(0..2))
+
+        val leftFirstIndex = "AAA = (".length
+        val left = NodeName(line.substring(leftFirstIndex, leftFirstIndex + 3))
+
+        val rightFirstIndex = line.length - "CCC)".length
+        val right = NodeName(line.substring(rightFirstIndex, rightFirstIndex + 3))
+
+        nodes += NodeData(name, left, right)
+      }
+    }
+
+    return Input(directions!!, nodes.associateBy { it.name })
   }
 
+  data class Input(
+    val directions: Directions,
+    val nodes: Map<NodeName, NodeData>,
+  )
+
   @JvmInline
-  value class Input(val value: String)
+  value class Directions(val directions: List<Direction>)
+
+  @JvmInline
+  value class NodeName(val name: String) {
+    init {
+      require(name.length == 3)
+    }
+  }
+
+  data class NodeData(val name: NodeName, val left: NodeName, val right: NodeName)
+
+  enum class Direction {
+    Left,
+    Right,
+  }
 }
--- src/test/kotlin/net/navatwo/adventofcode2023/day8/Day8SolutionTest.kt
+++ src/test/kotlin/net/navatwo/adventofcode2023/day8/Day8SolutionTest.kt
@@ -1,39 +1,118 @@
 package net.navatwo.adventofcode2023.day8
 
 import net.navatwo.adventofcode2023.benchmarks.Benchmark
+import net.navatwo.adventofcode2023.day8.Day8Solution.Direction
+import net.navatwo.adventofcode2023.day8.Day8Solution.Directions
+import net.navatwo.adventofcode2023.day8.Day8Solution.Input
+import net.navatwo.adventofcode2023.day8.Day8Solution.NodeData
+import net.navatwo.adventofcode2023.day8.Day8Solution.NodeName
 import net.navatwo.adventofcode2023.isComputed
 import net.navatwo.adventofcode2023.loadText
 import net.navatwo.adventofcode2023.parseResource
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Disabled
 import org.junit.jupiter.api.Test
 
 class Day8SolutionTest {
   @Test
-  @Disabled("TODO")
-  fun `p1 parse`() {
-    val resourceName = "day8/p1_sample.txt"
-    val solution = Day8Solution.Part1
-    val inputs = solution.parseResource(resourceName)
-    assertThat(inputs).isNotEmpty()
-  }
-
-  @Test
-  @Disabled("TODO")
-  fun `p1 sample`() {
-    val resourceName = "day8/p1_sample.txt"
+  fun `p1 parse sample 1`() {
+    val resourceName = "day8/p1_sample1.txt"
     val solution = Day8Solution.Part1
     val input = solution.parseResource(resourceName)
-    assertThat(Day8Solution.Part1.solve(input)).isComputed(32L)
+    assertThat(input).isEqualTo(
+      Input(
+        directions = Directions(listOf(Direction.Right, Direction.Left)),
+        nodes = mapOf(
+          NodeName("AAA") to NodeData(
+            name = NodeName("AAA"),
+            left = NodeName("BBB"),
+            right = NodeName("CCC"),
+          ),
+          NodeName("BBB") to NodeData(
+            name = NodeName("BBB"),
+            left = NodeName("DDD"),
+            right = NodeName("EEE"),
+          ),
+          NodeName("CCC") to NodeData(
+            name = NodeName("CCC"),
+            left = NodeName("ZZZ"),
+            right = NodeName("GGG"),
+          ),
+          NodeName("DDD") to NodeData(
+            name = NodeName("DDD"),
+            left = NodeName("DDD"),
+            right = NodeName("DDD"),
+          ),
+          NodeName("EEE") to NodeData(
+            name = NodeName("EEE"),
+            left = NodeName("EEE"),
+            right = NodeName("EEE"),
+          ),
+          NodeName("GGG") to NodeData(
+            name = NodeName("GGG"),
+            left = NodeName("GGG"),
+            right = NodeName("GGG"),
+          ),
+          NodeName("ZZZ") to NodeData(
+            name = NodeName("ZZZ"),
+            left = NodeName("ZZZ"),
+            right = NodeName("ZZZ"),
+          ),
+        ),
+      )
+    )
   }
 
   @Test
-  @Disabled("TODO")
+  fun `p1 parse sample 2`() {
+    val resourceName = "day8/p1_sample2.txt"
+    val solution = Day8Solution.Part1
+    val input = solution.parseResource(resourceName)
+    assertThat(input).isEqualTo(
+      Input(
+        directions = Directions(listOf(Direction.Left, Direction.Left, Direction.Right)),
+        nodes = mapOf(
+          NodeName("AAA") to NodeData(
+            name = NodeName("AAA"),
+            left = NodeName("BBB"),
+            right = NodeName("BBB"),
+          ),
+          NodeName("BBB") to NodeData(
+            name = NodeName("BBB"),
+            left = NodeName("AAA"),
+            right = NodeName("ZZZ"),
+          ),
+          NodeName("ZZZ") to NodeData(
+            name = NodeName("ZZZ"),
+            left = NodeName("ZZZ"),
+            right = NodeName("ZZZ"),
+          ),
+        ),
+      )
+    )
+  }
+
+  @Test
+  fun `p1 sample 1`() {
+    val resourceName = "day8/p1_sample1.txt"
+    val solution = Day8Solution.Part1
+    val input = solution.parseResource(resourceName)
+    assertThat(Day8Solution.Part1.solve(input)).isComputed(2)
+  }
+
+  @Test
+  fun `p1 sample 2`() {
+    val resourceName = "day8/p1_sample2.txt"
+    val solution = Day8Solution.Part1
+    val input = solution.parseResource(resourceName)
+    assertThat(Day8Solution.Part1.solve(input)).isComputed(6)
+  }
+
+  @Test
   fun `p1`() {
     val resourceName = "day8/p1_input.txt"
     val solution = Day8Solution.Part1
     val input = solution.parseResource(resourceName)
-    assertThat(Day8Solution.Part1.solve(input)).isComputed(32L)
+    assertThat(Day8Solution.Part1.solve(input)).isComputed(6)
 
     Benchmark.run(
       inputContent = loadText(resourceName),
