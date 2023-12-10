22f9e18be1f4eff06db833b40ebee2291a8e920b
Kevin Brightwell <kevin.brightwell@faire.com>
2023-12-10 08:52:18 -0500

    parse

--- src/main/kotlin/net/navatwo/adventofcode2023/day10/Day10Solution.kt
+++ src/main/kotlin/net/navatwo/adventofcode2023/day10/Day10Solution.kt
@@ -1,20 +1,46 @@
 package net.navatwo.adventofcode2023.day10
 
+import net.navatwo.adventofcode2023.Grid
 import net.navatwo.adventofcode2023.framework.ComputedResult
 import net.navatwo.adventofcode2023.framework.Solution
 
-sealed class Day10Solution : Solution<List<Day10Solution.Input>> {
+sealed class Day10Solution : Solution<Grid<Day10Solution.Tile>> {
   data object Part1 : Day10Solution() {
-    override fun solve(input: List<Input>): ComputedResult {
+    override fun solve(input: Grid<Tile>): ComputedResult {
       // TODO Implement
       return ComputedResult.Simple(32L)
     }
   }
 
-  override fun parse(lines: Sequence<String>): List<Input> {
-    return lines.map { Input(it) }.toList()
+  override fun parse(lines: Sequence<String>): Grid<Tile> {
+    val tiles = lines.filter { it.isNotBlank() }
+      .map { line ->
+        line.mapTo(mutableListOf()) { Tile.fromChar(it) }
+      }
+      .toMutableList()
+
+    return Grid(tiles)
   }
 
   @JvmInline
   value class Input(val value: String)
+
+  enum class Tile(val char: Char) {
+    Vertical('|'),
+    Horizontal('-'),
+    NENinety('L'),
+    NWNinety('J'),
+    SWNinety('7'),
+    SENinety('F'),
+    Ground('.'),
+    Start('S'),
+    ;
+
+    companion object {
+      private val charToTile = entries.associateBy { it.char }
+      fun fromChar(char: Char): Tile {
+        return charToTile.getValue(char)
+      }
+    }
+  }
 }
--- src/test/kotlin/net/navatwo/adventofcode2023/day10/Day10SolutionTest.kt
+++ src/test/kotlin/net/navatwo/adventofcode2023/day10/Day10SolutionTest.kt
@@ -1,6 +1,13 @@
 package net.navatwo.adventofcode2023.day10
 
 import net.navatwo.adventofcode2023.benchmarks.Benchmark
+import net.navatwo.adventofcode2023.day10.Day10Solution.Tile.Ground
+import net.navatwo.adventofcode2023.day10.Day10Solution.Tile.Horizontal
+import net.navatwo.adventofcode2023.day10.Day10Solution.Tile.NENinety
+import net.navatwo.adventofcode2023.day10.Day10Solution.Tile.NWNinety
+import net.navatwo.adventofcode2023.day10.Day10Solution.Tile.SENinety
+import net.navatwo.adventofcode2023.day10.Day10Solution.Tile.SWNinety
+import net.navatwo.adventofcode2023.day10.Day10Solution.Tile.Vertical
 import net.navatwo.adventofcode2023.isComputed
 import net.navatwo.adventofcode2023.loadText
 import net.navatwo.adventofcode2023.parseResource
@@ -13,8 +20,14 @@ class Day10SolutionTest {
   fun `p1 parse`() {
     val resourceName = "day10/p1_sample.txt"
     val solution = Day10Solution.Part1
-    val inputs = solution.parseResource(resourceName)
-    assertThat(inputs).isNotEmpty()
+    val grid = solution.parseResource(resourceName)
+    assertThat(grid.rows).containsExactly(
+      listOf(Ground, Ground, Ground, Ground, Ground),
+      listOf(Ground, SENinety, Horizontal, SWNinety, Ground),
+      listOf(Ground, Vertical, Ground, Vertical, Ground),
+      listOf(Ground, NENinety, Horizontal, NWNinety, Ground),
+      listOf(Ground, Ground, Ground, Ground, Ground),
+    )
   }
 
   @Test
